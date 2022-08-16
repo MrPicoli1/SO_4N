beb102f768cea3cc15f4c7a5bf4d6675ced456f4
CaioSampaio <39320152+CaioSampaio@users.noreply.github.com>
2022-08-16 11:49:16 -0300

    Update Lab1a.c

--- Lab01/Lab1a.c
+++ Lab01/Lab1a.c
@@ -1,3 +1,14 @@
+/*
+EXERCÍCIO DE SO – TURMA 04N
+
+Nomes:
+Caio Sampaio Oliveira, TIA: 32195621.
+Guilherme Picoli Rodrigues, TIA 32147317;
+Leonardo Henrique de Oliveira Matos, TIA 32139561.
+
+Atividade Lab 01a - 11/08/22
+*/
+
 #include <stdio.h>
 
 int main(void) {
