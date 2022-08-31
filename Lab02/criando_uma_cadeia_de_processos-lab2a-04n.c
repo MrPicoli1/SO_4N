6ff63db2b6f53aeabfea983aa37fac9e5927d44d
Caio Sampaio <39320152+CaioSampaio@users.noreply.github.com>
2022-08-31 19:13:04 -0300

    Update criando_uma_cadeia_de_processos-lab2a-04n.c

--- Lab02/criando_uma_cadeia_de_processos-lab2a-04n.c
+++ Lab02/criando_uma_cadeia_de_processos-lab2a-04n.c
@@ -6,7 +6,7 @@ int main (int argc, char *argv[]){
   pid_t childpid = 0;
   int i, n;
   if (argc != 2){ /* check for valid number of command-line arguments */
-    fprintf(stderr, "Usage: %s processes\n", argv[0]);
+    fprintf(stdout, "Usage: %s processes\n", argv[0]);
     return 1;
 
 
@@ -16,7 +16,7 @@ int main (int argc, char *argv[]){
   for (i = 1; i < n; i++)
     if (childpid = fork())
       break;
-  fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n",
+  fprintf(stdout, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n",
       i, (long)getpid(), (long)getppid(), (long)childpid);
   return 0;
 }
